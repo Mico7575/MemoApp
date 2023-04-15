bce2f6f3d9d4d4224b283761d3ec64649a36626e
Mico <amurom72@yahoo.co.jp>
2023-04-15 15:28:34 +0900

    Creat Hello component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import react from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
+    // bang={true}のtrueは省略できる。bangのみ
     <View style={styles.container}>
+      <Hello bang={true}>World</Hello>
+      <Hello style={{ fontSize: 16 }}>Hello Small World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/components/Hello.jsx
@@ -0,0 +1,41 @@
+import react from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { string, bool, shape } from "prop-types";
+
+function Hello(props) {
+  //const children = props.children; 以下と同じ。
+  const { children, bang, style } = props;
+  return (
+    //バッククォート(``)と${}を用いた場合は${children}変数を使用することを意味する。
+    // バッククォート使用しない場合：<Text style={styles.text}>Hello {children}</Text>
+    <View>
+      <Text style={[styles.text, style]}>{`Hello
+      ${children}${bang ? "!" : ""}`}</Text>
+    </View>
+  );
+}
+
+// prop-typesで型を定義する。
+Hello.propTypes = {
+  children: string.isRequired,
+  //string型でありhelloを使う時にchildrenを渡すのが必須となる。（isRequired）
+  bang: bool,
+  style: shape(),
+};
+
+Hello.defaultProps = {
+  bang: false,
+  style: null,
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#ffffff",
+    backgroundColor: "#0000ff",
+    fontSize: 40,
+    fontWeight: "bold",
+    padding: 16,
+  },
+});
+
+export default Hello;
